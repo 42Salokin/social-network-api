14e832449ef6d69c709acb5fc99785b85963771a
Salokin <nicholas.dpoul@gmail.com>
2024-05-27 12:19:45 -0400

    created and tested route to add user to other user's friends list

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -72,7 +72,25 @@ const userController = {
   },
 
   // add friend to friend list
-  async addFriend(req, res) {},
+  async addFriend(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: { friends: req.params.friendId } },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: 'No user found with that ID :(' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
   // remove friend from friend list
   async removeFriend(req, res) {},
 };
